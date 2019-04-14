3cde44dd7bc8c2b7c8656ad19a2acbc49249740b
Manuel Fernandez <manferlo81@gmail.com>
2019-04-14 14:25:43 +0700

    expose internal API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { default as bundlib } from "./bundlib";
+export { default as analizePkg } from "./analize-pkg";
+export { default as pkgToConfigs } from "./pkg-to-configs";
+export { default as rollItUp } from "./roll-it-up";
