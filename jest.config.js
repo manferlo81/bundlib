dd561681cbf9df3e90b2dc669131f6585296c287
Manuel Fernandez <manferlo81@gmail.com>
2019-04-14 14:21:37 +0700

    test setup

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+  testEnvironment: "node",
+
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: [
+    "json",
+    "text"
+  ],
+
+};
---
+++ test/test.js
@@ -0,0 +1,3 @@
+test("dummy test", () => {
+  expect(2).toBe(2);
+});
