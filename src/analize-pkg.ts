5da73e266ee11fd7a9a5dcf67039c61a7bf6804a
Manuel Fernandez <manferlo81@gmail.com>
2019-04-14 20:09:09 +0700

    fixed a problem with type definition dir

--- src/analize-pkg.ts
+++ src/analize-pkg.ts
@@ -1,5 +1,5 @@
 import mod from "module";
-import { resolve as resolvePath } from "path";
+import { parse, resolve as resolvePath } from "path";
 
 import getPkg from "./get-pkg";
 import { BundlibBuildOptions, BundlibPkgOptions, Pkg } from "./pkg";
@@ -21,6 +21,18 @@ const analizePkg = async (cwd: string, pkg?: Pkg): Promise<BundlibPkg> => {
 
   let types: string | null = pkgTypes || typings || null;
   types = types && resolvePath(cwd, types);
+
+  if (types) {
+
+    const { dir, ext } = parse(types);
+
+    // in case types points to folder/index.d.ts
+    // set types to folder
+    if (ext === ".ts") {
+      types = dir;
+    }
+
+  }
 
   let { input, sourcemap, esModule, interop, iife, amd, umd, name, id, extend } = bundlib || {} as BundlibPkgOptions;
 
