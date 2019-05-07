8d55ce7195f5a08c576a11ed29185f64b47883d7
Manuel Fernandez <manferlo81@gmail.com>
2019-05-07 19:14:27 +0700

    test bundlib with options

--- test/bundlib/bundlib.test.js
+++ test/bundlib/bundlib.test.js
@@ -31,4 +31,31 @@ describe("bundlib", () => {
 
   }, 10000);
 
+  test("bundlib (with options) should generates files", async (done) => {
+
+    const projectCwd = resolve(process.cwd(), "test/projects/1");
+    const files = ["lib.js", "lib.d.ts"].map(
+      (fn) => resolve(projectCwd, fn)
+    );
+
+    const writtenListener = jest.fn();
+
+    const build = await bundlib(projectCwd, {
+      dev: true,
+    });
+
+    build.on("WRITTEN", writtenListener);
+    build.on("BUILT", () => {
+
+      expect(writtenListener).toHaveBeenCalledTimes(files.length);
+      files.forEach((filename, index) => {
+        expect(writtenListener).toHaveBeenNthCalledWith(index + 1, filename);
+      });
+
+      done();
+
+    });
+
+  }, 10000);
+
 });
