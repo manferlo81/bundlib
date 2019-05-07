34d52418e4c299382bc0bd8ed2d0cf2267a570b2
Manuel Fernandez <manferlo81@gmail.com>
2019-05-07 17:47:32 +0700

    mock fs write

--- test/bundlib/bundlib.test.js
+++ test/bundlib/bundlib.test.js
@@ -1,23 +1,31 @@
-const { bundlib } = require("../..");
 const { resolve } = require("path");
-const { cleanup, exist } = require("../test-helpers/fs");
+
+require("../mock/mock-file-write");
+const { bundlib } = require("../..");
 
 describe("bundlib", () => {
 
   test("bundlib should generates 3 files", async (done) => {
 
     const projectCwd = resolve(process.cwd(), "test/projects/1");
-    const files = ["lib.js", "lib.js.map", "lib.d.ts"].map(
+    const files = ["lib.js", "lib.d.ts"].map(
       (fn) => resolve(projectCwd, fn)
     );
 
-    cleanup(files);
+    const writtenListener = jest.fn();
 
     const build = await bundlib(projectCwd);
 
+    build.on("WRITTEN", writtenListener);
     build.on("BUILT", () => {
-      exist(files);
+
+      expect(writtenListener).toHaveBeenCalledTimes(files.length);
+      files.forEach((filename, index) => {
+        expect(writtenListener).toHaveBeenNthCalledWith(index + 1, filename);
+      });
+
       done();
+
     });
 
   }, 10000);
---
+++ test/mock/mock-file-write.js
@@ -0,0 +1,18 @@
+jest.mock("fs", () => {
+
+  let fs = jest.requireActual("fs");
+
+  function writeFileSync() { }
+
+  function writeFile(filename, content, callback) {
+    setImmediate(() => {
+      writeFileSync(filename, content);
+      callback(null);
+    });
+  }
+
+  return { ...fs, writeFileSync, writeFile };
+
+});
+
+module.exports = require("fs");
