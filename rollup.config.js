046743e855a6145d6df581bcf7587c03cf9c0b4b
Manuel Fernandez <manferlo81@gmail.com>
2019-05-24 19:57:38 +0700

    build cli using rollup

---
+++ rollup.config.js
@@ -0,0 +1,90 @@
+import builtinModules from "builtin-modules";
+
+import ts2 from "rollup-plugin-typescript2";
+import json from "rollup-plugin-json";
+import stripShebang from "rollup-plugin-strip-shebang";
+import addShebang from "rollup-plugin-add-shebang";
+import babel from "rollup-plugin-babel";
+import buble from "rollup-plugin-buble";
+
+import { bin, dependencies } from "./package.json";
+
+const input = "src-bin/index.ts";
+
+const runtimeDependencies = Object.keys(dependencies);
+const external = [...builtinModules, ...runtimeDependencies];
+
+let capturedShebang = null;
+
+/** @type { import("rollup").RollupOptions } */
+const config = {
+
+  input,
+
+  output: {
+    file: bin,
+    format: "cjs",
+    sourcemap: false,
+    esModule: false,
+    interop: false,
+  },
+
+  external,
+
+  plugins: [
+
+    stripShebang({
+      capture(shebangFromFile) {
+        capturedShebang = shebangFromFile;
+      },
+    }),
+
+    {
+      resolveId(id) {
+        if (id === "../src") {
+          return {
+            id: "..",
+            external: true,
+          };
+        }
+      },
+    },
+
+    ts2({
+      cacheRoot: ".cache/rpt2",
+      tsconfigOverride: {
+        compilerOptions: {
+          target: "esnext",
+          module: "esnext",
+          declaration: false,
+        },
+      },
+    }),
+
+    json(),
+
+    babel({
+      extensions: [".ts", ".js"],
+      exclude: /node_modules/,
+      babelrc: false,
+      plugins: ["babel-plugin-transform-async-to-promises"],
+    }),
+
+    buble({
+      exclude: /node_modules/,
+      target: {
+        node: 0.12,
+      },
+      objectAssign: true,
+    }),
+
+    addShebang({
+      shebang: capturedShebang || "#!/usr/bin/env node",
+      include: bin,
+    }),
+
+  ],
+
+};
+
+export default config;
