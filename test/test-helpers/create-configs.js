ec6df3ca4e0b825922930a202f08d3b339df664b
Manuel Fernandez <manferlo81@gmail.com>
2019-05-30 18:41:31 +0700

    adjusted test for new features

--- test/test-helpers/create-configs.js
+++ test/test-helpers/create-configs.js
@@ -3,7 +3,9 @@ const { pkgToConfigs } = require("../..");
 
 async function createConfigs(cwd, dev, pkgjson) {
   const pkg = pkgjson ? await analize(cwd, pkgjson) : await analize(cwd);
-  return pkgToConfigs(pkg, dev);
+  return pkgToConfigs(pkg, {
+    dev,
+  });
 }
 
 module.exports = createConfigs;
