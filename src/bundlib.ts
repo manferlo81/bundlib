bf13c06fe1ec157f486504ad9e63dd2a98ecc65a
Manuel Fernandez <manferlo81@gmail.com>
2019-05-31 23:47:23 +0700

    bundlib show version 0.4

--- src/bundlib.ts
+++ src/bundlib.ts
@@ -8,7 +8,7 @@ import { BundlibOptions } from "./types";
 // import { version } from "../package.json";
 // version has to be hardcoded due to issue #7
 // https://github.com/manferlo81/bundlib/issues/7
-const version = 0.3;
+const version = 0.4;
 
 const bundlib = async (cwd: string, { dev, watch, silent }: BundlibOptions = {}) => {
 
