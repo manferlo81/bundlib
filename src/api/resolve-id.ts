3a8eef6d6032dd10ba071e342c4db5bdaeec5883
Manuel Fernandez <manferlo81@gmail.com>
2019-06-27 16:01:26 +0700

    edited

--- src/api/resolve-id.ts
+++ src/api/resolve-id.ts
@@ -1,6 +1,6 @@
 import { dirname, isAbsolute, join, normalize } from "path";
 
-function resolveId(id: string, cwd: string, from: string | undefined) {
+function resolveId(id: string, cwd: string, from?: string) {
   return isAbsolute(id) ? id : normalize(join(
     from ? dirname(from) : cwd,
     id,
