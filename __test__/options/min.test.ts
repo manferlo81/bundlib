5ad6fbcdf15d23290d5cf5dc652e4a9d042ed1ca
Manuel Fernandez <manferlo81@gmail.com>
2019-07-11 03:12:09 +0700

    added tests for some per-build options

--- __test__/options/min.test.ts
+++ __test__/options/min.test.ts
@@ -1,3 +1,4 @@
+import { MinifiableFields } from "../../src/api/pkg-analized";
 import analize from "../tools/analize";
 
 describe("min option", () => {
@@ -10,6 +11,14 @@ describe("min option", () => {
     browser: "lib.js",
     bin: "lib.js",
     bundlib: { min },
+  });
+
+  const analizeWithBuildMin = (min: any, field: MinifiableFields) => analize(cwd, {
+    main: "lib.js",
+    module: "lib.js",
+    browser: "lib.js",
+    bin: "lib.js",
+    bundlib: { [field]: { min } },
   });
 
   test("should throw on invalid min option", () => {
@@ -71,6 +80,66 @@ describe("min option", () => {
       .toEqual(true);
     expect(analized.output.browser ? analized.output.browser.min : null)
       .toEqual(true);
+    expect(analized.output.bin ? analized.output.bin.min : null)
+      .toEqual(true);
+
+  });
+
+  test("should work with build min option on CommonJS module", async () => {
+
+    const analized = await analizeWithBuildMin(true, "main");
+
+    expect(analized.output.main ? analized.output.main.min : null)
+      .toEqual(true);
+    expect(analized.output.module ? analized.output.module.min : null)
+      .toEqual(false);
+    expect(analized.output.browser ? analized.output.browser.min : null)
+      .toEqual(false);
+    expect(analized.output.bin ? analized.output.bin.min : null)
+      .toEqual(false);
+
+  });
+
+  test("should work with build min option on ESModule", async () => {
+
+    const analized = await analizeWithBuildMin(true, "module");
+
+    expect(analized.output.main ? analized.output.main.min : null)
+      .toEqual(false);
+    expect(analized.output.module ? analized.output.module.min : null)
+      .toEqual(true);
+    expect(analized.output.browser ? analized.output.browser.min : null)
+      .toEqual(false);
+    expect(analized.output.bin ? analized.output.bin.min : null)
+      .toEqual(false);
+
+  });
+
+  test("should work with build min option on browser module", async () => {
+
+    const analized = await analizeWithBuildMin(true, "browser");
+
+    expect(analized.output.main ? analized.output.main.min : null)
+      .toEqual(false);
+    expect(analized.output.module ? analized.output.module.min : null)
+      .toEqual(false);
+    expect(analized.output.browser ? analized.output.browser.min : null)
+      .toEqual(true);
+    expect(analized.output.bin ? analized.output.bin.min : null)
+      .toEqual(false);
+
+  });
+
+  test("should work with build min option on binary module", async () => {
+
+    const analized = await analizeWithBuildMin(true, "bin");
+
+    expect(analized.output.main ? analized.output.main.min : null)
+      .toEqual(false);
+    expect(analized.output.module ? analized.output.module.min : null)
+      .toEqual(false);
+    expect(analized.output.browser ? analized.output.browser.min : null)
+      .toEqual(false);
     expect(analized.output.bin ? analized.output.bin.min : null)
       .toEqual(true);
 
--- __test__/options/sourcemap.test.ts
+++ __test__/options/sourcemap.test.ts
@@ -4,6 +4,22 @@ describe("sourcemap option", () => {
 
   const cwd = process.cwd();
 
+  const analizedWithSourcemap = (sourcemap: any) => analize(cwd, {
+    main: "out/lib.cjs.js",
+    module: "out/lib.es.js",
+    browser: "out/lib.umd.js",
+    bin: "out/lib.bin.js",
+    bundlib: { sourcemap },
+  });
+
+  const analizedWithBuildSourcemap = (field: any, sourcemap: any) => analize(cwd, {
+    main: "out/lib.cjs.js",
+    module: "out/lib.es.js",
+    browser: "out/lib.umd.js",
+    bin: "out/lib.bin.js",
+    bundlib: { [field]: { sourcemap } },
+  });
+
   test("should throw on invalid sourcemap option", () => {
 
     const invalidSoucemaps = [
@@ -16,23 +32,17 @@ describe("sourcemap option", () => {
     expect.assertions(invalidSoucemaps.length);
 
     invalidSoucemaps.forEach((sourcemap) => {
-      // @ts-ignore
-      expect(analize(cwd, {
-        bundlib: { sourcemap },
-      })).rejects.toThrow(TypeError);
+      expect(
+        analizedWithSourcemap(sourcemap),
+      ).rejects
+        .toThrow(TypeError);
     });
 
   });
 
   test("should read sourcemap option", async () => {
 
-    const analized = await analize(cwd, {
-      main: "out/lib.cjs.js",
-      module: "out/lib.es.js",
-      browser: "out/lib.umd.js",
-      bin: "out/lib.bin.js",
-      bundlib: { sourcemap: false },
-    });
+    const analized = await analizedWithSourcemap(false);
 
     expect(analized.output.main ? analized.output.main.sourcemap : null).toBe(false);
     expect(analized.output.module ? analized.output.module.sourcemap : null).toBe(false);
@@ -43,18 +53,34 @@ describe("sourcemap option", () => {
 
   test("should read inline sourcemap option", async () => {
 
-    const analized = await analize(cwd, {
-      main: "out/lib.cjs.js",
-      module: "out/lib.es.js",
-      browser: "out/lib.umd.js",
-      bin: "out/lib.bin.js",
-      bundlib: { sourcemap: "inline" },
-    });
+    const analized = await analizedWithSourcemap("inline");
 
     expect(analized.output.main ? analized.output.main.sourcemap : null).toBe("inline");
     expect(analized.output.module ? analized.output.module.sourcemap : null).toBe("inline");
     expect(analized.output.browser ? analized.output.browser.sourcemap : null).toBe("inline");
     expect(analized.output.bin ? analized.output.bin.sourcemap : null).toBe("inline");
+
+  });
+
+  test("should read build sourcemap option", async () => {
+
+    const analized = await analizedWithBuildSourcemap("main", false);
+
+    expect(analized.output.main ? analized.output.main.sourcemap : null).toBe(false);
+    expect(analized.output.module ? analized.output.module.sourcemap : null).toBe(true);
+    expect(analized.output.browser ? analized.output.browser.sourcemap : null).toBe(true);
+    expect(analized.output.bin ? analized.output.bin.sourcemap : null).toBe(true);
+
+  });
+
+  test("should read build inline sourcemap option", async () => {
+
+    const analized = await analizedWithBuildSourcemap("module", "inline");
+
+    expect(analized.output.main ? analized.output.main.sourcemap : null).toBe(true);
+    expect(analized.output.module ? analized.output.module.sourcemap : null).toBe("inline");
+    expect(analized.output.browser ? analized.output.browser.sourcemap : null).toBe(true);
+    expect(analized.output.bin ? analized.output.bin.sourcemap : null).toBe(true);
 
   });
 
