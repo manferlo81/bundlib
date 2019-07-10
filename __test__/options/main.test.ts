2b4fd7a7a6920770f475f801fe2e4df4a5bccc9a
Manuel Fernandez <manferlo81@gmail.com>
2019-07-11 02:14:25 +0700

    more tests

---
+++ __test__/options/main.test.ts
@@ -0,0 +1,41 @@
+import analize from "../tools/analize";
+
+describe("main option", () => {
+
+  const cwd = process.cwd();
+
+  const analizeWithMain = (main: any) => analize(cwd, {
+    main: "out.js",
+    bundlib: { main },
+  });
+
+  test("should throw on invalid main option", () => {
+
+    const invalidMain = [
+      1,
+      true,
+      { invalid: true },
+      { api: 10, bin: 11 },
+    ];
+
+    expect.assertions(invalidMain.length);
+
+    invalidMain.forEach((main) => {
+      expect(
+        analizeWithMain(main),
+      ).rejects
+        .toThrow(TypeError);
+    });
+
+  });
+
+  test("should prevent CommonJS module build if main = false", async () => {
+
+    const analized = await analizeWithMain(false);
+
+    expect(analized.output.main)
+      .toBeNull();
+
+  });
+
+});
