835120678ee835a2f1e6a4abedf827a769523273
Manuel Fernandez <manferlo81@gmail.com>
2019-07-11 03:12:09 +0700

    added tests for some per-build options

--- __test__/options/format.test.ts
+++ __test__/options/format.test.ts
@@ -4,6 +4,16 @@ describe("format option", () => {
 
   const cwd = process.cwd();
 
+  const analizeWithFormat = (format: any) => analize(cwd, {
+    browser: "out/lib.js",
+    bundlib: { format },
+  });
+
+  const analizeWithBuildFormat = (format: any) => analize(cwd, {
+    browser: "out/lib.js",
+    bundlib: { browser: { format } },
+  });
+
   test("should throw on invalid format option", () => {
 
     const invalidBrowserFormats = [
@@ -14,10 +24,10 @@ describe("format option", () => {
     expect.assertions(invalidBrowserFormats.length);
 
     invalidBrowserFormats.forEach((format) => {
-      // @ts-ignore
-      expect(analize(cwd, {
-        bundlib: { format },
-      })).rejects.toThrow(TypeError);
+      expect(
+        analizeWithFormat(format),
+      ).rejects
+        .toThrow(TypeError);
     });
 
   });
@@ -26,12 +36,10 @@ describe("format option", () => {
 
     const format = "iife";
 
-    const analized = await analize(cwd, {
-      browser: "out/lib.js",
-      bundlib: { format },
-    });
+    const analized = await analizeWithFormat(format);
 
-    expect(analized.output.browser ? analized.output.browser.format : null).toBe(format);
+    expect(analized.output.browser ? analized.output.browser.format : null)
+      .toBe(format);
 
   });
 
@@ -39,12 +47,10 @@ describe("format option", () => {
 
     const format = "amd";
 
-    const analized = await analize(cwd, {
-      browser: "out/lib.js",
-      bundlib: { format },
-    });
+    const analized = await analizeWithFormat(format);
 
-    expect(analized.output.browser ? analized.output.browser.format : null).toBe(format);
+    expect(analized.output.browser ? analized.output.browser.format : null)
+      .toBe(format);
 
   });
 
@@ -52,22 +58,65 @@ describe("format option", () => {
 
     const format = "umd";
 
-    const analized = await analize(cwd, {
-      browser: "out/lib.js",
-      bundlib: { format },
-    });
+    const analized = await analizeWithFormat(format);
 
-    expect(analized.output.browser ? analized.output.browser.format : null).toBe(format);
+    expect(analized.output.browser ? analized.output.browser.format : null)
+      .toBe(format);
+
+  });
+
+  test("should set browser format to iife from build", async () => {
+
+    const format = "iife";
+
+    const analized = await analizeWithBuildFormat(format);
+
+    expect(analized.output.browser ? analized.output.browser.format : null)
+      .toBe(format);
+
+  });
+
+  test("should set browser format to amd from build", async () => {
+
+    const format = "amd";
+
+    const analized = await analizeWithBuildFormat(format);
+
+    expect(analized.output.browser ? analized.output.browser.format : null)
+      .toBe(format);
+
+  });
+
+  test("should set browser format to umd from build", async () => {
+
+    const format = "umd";
+
+    const analized = await analizeWithBuildFormat(format);
+
+    expect(analized.output.browser ? analized.output.browser.format : null)
+      .toBe(format);
 
   });
 
   test("should default to umd in no browser format provided", async () => {
 
-    const analized = await analize(cwd, {
-      browser: "out/lib.js",
-    });
+    const analized = await analize(cwd, { browser: "out/lib.js" });
 
     expect(analized.output.browser ? analized.output.browser.format : null).toBe("umd");
+
+  });
+
+  test("should read build browser format over global one", async () => {
+
+    const analized = await analize(cwd, {
+      browser: "out/lib.js",
+      bundlib: {
+        format: "amd",
+        browser: { format: "iife" },
+      },
+    });
+
+    expect(analized.output.browser ? analized.output.browser.format : null).toBe("iife");
 
   });
 
