6ead0947519f6b9e8be9acfe5320d5c7c447a40a
Manuel Fernandez <manferlo81@gmail.com>
2019-07-11 12:49:31 +0700

    removed unnecessary method

--- src/api/validate-options.ts
+++ src/api/validate-options.ts
@@ -1,44 +1,6 @@
-import { BundlibOptions10 } from "./bundlib-options";
 import keys from "./obj-keys";
 
 export function invalidKeys(object: Record<string, any>, list: string[]): string[] | null {
   const invalid = keys(object).filter((key) => list.indexOf(key) === -1);
   return invalid.length ? invalid : null;
-}
-
-export function getInvalidOptions(options: Record<string, any>): string[] | null {
-  return invalidKeys(options, [
-    "input",
-    "extend",
-    "esModule",
-    "interop",
-    "equals",
-    "sourcemap",
-    "format",
-    "name",
-    "id",
-    "globals",
-    "min",
-    "cache",
-    "main",
-    "module",
-    "browser",
-    "bin",
-    "types",
-  ] as Array<keyof BundlibOptions10>);
-  // return keys(options as Record<keyof BundlibOptions10, any>).filter((name) => (
-  //   name !== "input" &&
-  //   name !== "bin" &&
-  //   name !== "extend" &&
-  //   name !== "esModule" &&
-  //   name !== "interop" &&
-  //   name !== "equals" &&
-  //   name !== "sourcemap" &&
-  //   name !== "format" &&
-  //   name !== "name" &&
-  //   name !== "id" &&
-  //   name !== "globals" &&
-  //   name !== "min" &&
-  //   name !== "cache"
-  // ));
 }
