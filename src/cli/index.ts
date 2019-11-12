e4a2cd9dcc40bc5fd1d123b4698352da4ced2847
Manuel Fernandez <manferlo81@gmail.com>
2019-11-12 16:58:07 +0700

    fix: show correct name on cli help

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,20 +1,13 @@
 #!/usr/bin/env node
 
 import program from 'commander'
-import { displayName, version } from '../../package.json'
+import { name, displayName, version } from '../../package.json'
 
 import { action } from './action'
 
-declare module 'commander' {
-  interface CommanderStatic {
-    dev: boolean;
-    watch: boolean;
-    silent: boolean;
-  }
-}
-
 program
   .version(version, '-v, --version')
+  .name(name)
   .option('-d, --dev', 'create development builds')
   .option('-w, --watch', 'run bundlib in watch mode')
   .option('-s, --silent', 'prevent messages from showing in the console')
