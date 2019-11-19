1a67113978730767046aed1dcbf289aa6c62ab44
Manuel Fernandez <manferlo81@gmail.com>
2019-11-19 20:55:23 +0700

    fixed isDepInstalled return

--- src/api/is-dep-installed.ts
+++ src/api/is-dep-installed.ts
@@ -1,7 +1,7 @@
 function isDepInstalled(name: string, ...list: Array<string[] | null>) {
-  return list.some((deps) => {
+  return list.some((deps) => (
     deps && deps.indexOf(name) !== -1
-  })
+  ))
 }
 
 export default isDepInstalled
