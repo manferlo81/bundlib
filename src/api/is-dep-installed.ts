34641493a6807ae5923d0ca6198ed1d06f491233
Manuel Fernandez <manferlo81@gmail.com>
2020-03-16 20:59:50 +0700

    isDepInstalled returns boolean

--- src/api/is-dep-installed.ts
+++ src/api/is-dep-installed.ts
@@ -1,14 +1,7 @@
-export function isDepInstalled(name: string, ...list: Array<Record<string, string> | null>): string | null {
-  return list.reduce<string | null>((result, deps) => {
-    if (!deps) {
-      return result
-    }
-    const ver = deps[name] as string | void
-    if (!ver) {
-      return result
-    }
-    return ver
-  }, null)
+function isDepInstalled(name: string, ...list: Array<Record<string, string> | null>): boolean {
+  return list.some(
+    (deps) => deps && Object.prototype.hasOwnProperty.call(deps, name),
+  )
 }
 
 export default isDepInstalled
