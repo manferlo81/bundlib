d19fd46bba532eb1d3ed8e8f9402ffce6c8fea38
Manuel Fernandez <manferlo81@gmail.com>
2020-08-02 21:56:08 +0700

    fix: Crash if peer deps not installed

--- src/api/modules.d.ts
+++ src/api/modules.d.ts
@@ -1,22 +1,3 @@
-declare module '@rollup/plugin-babel' {
-
-  import { PluginImpl } from 'rollup';
-
-  type MinimatchPattern = Array<string | RegExp> | string | RegExp | null;
-
-  export interface BabelPluginOptions {
-    include?: MinimatchPattern;
-    exclude?: MinimatchPattern;
-    extensions?: string[];
-    babelHelpers?: 'bundled' | 'runtime' | 'inline' | 'external';
-  }
-
-  const babel: PluginImpl<BabelPluginOptions>;
-
-  export default babel;
-
-}
-
 declare module 'rollup-plugin-eslint' {
 
   import { PluginImpl } from 'rollup';
