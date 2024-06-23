8b8aaa57463ba6e91960ccbb93abda481f7d6dc2
Manuel Fernández <manferlo81@gmail.com>
2024-06-23 15:47:54 +0700

    chore: Add help script

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,17 +1,18 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import { displayName, name, version } from '../../package.json';
+import { displayName, name as libraryName, version as libraryVersion } from '../../package.json';
 import { action } from './action';
 
 void program
-  .version(version, '-v, --version')
-  .name(name)
+  .version(libraryVersion, '-v, --version')
+  .name(libraryName)
+  .helpOption('-h, --help', 'Display this help screen')
   .option('-d, --dev', 'create development builds')
   .option('-w, --watch', 'run bundlib in watch mode')
   .option('-s, --silent', 'prevent messages from showing in the console')
   .action(async () => {
     const { dev, watch, silent } = program.opts<{ dev: boolean; watch: boolean; silent: boolean }>();
-    await action(displayName, version, dev, watch, silent);
+    await action(displayName, libraryVersion, dev, watch, silent);
   })
   .parseAsync(process.argv);
