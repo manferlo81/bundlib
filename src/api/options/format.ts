5650f4d9bb99db136f1a2a23b803d8948fffd04b
Manuel Fernández <manferlo81@gmail.com>
2024-08-08 12:40:48 +0700

    chore: Cleanup `globals` option tools

--- src/api/options/format.ts
+++ src/api/options/format.ts
@@ -1,11 +1,11 @@
 import { composeOneOf, createOneOfLiteral } from '../type-check/advanced';
 import { isNullish } from '../type-check/basic';
-import type { AllowNullish } from '../types/helper-types';
+import type { Nullish } from '../types/helper-types';
 import type { BrowserBuildFormat } from '../types/rollup';
 
 const isBrowserFormat = createOneOfLiteral<BrowserBuildFormat>('iife', 'amd', 'umd');
 
-export const isBrowserFormatOrNullish = composeOneOf<AllowNullish<BrowserBuildFormat>>(
+export const isBrowserFormatOrNullish = composeOneOf<BrowserBuildFormat | Nullish>(
   isNullish,
   isBrowserFormat,
 );
--- src/api/options/globals.ts
+++ src/api/options/globals.ts
@@ -5,18 +5,34 @@ import type { GlobalsOptions } from '../types/bundlib-options';
 import { AllowNull } from '../types/helper-types';
 
 export function isValidGlobals(value: unknown): value is GlobalsOptions {
+
+  // return true if value is nullish
   if (isNullish(value)) return true;
+
+  // return false if value is not an object or an array
   if (!isObject(value)) return false;
-  if (isArray(value)) return value.every((item) => isString(item));
+
+  // if value is an array return whether or not every item is a string
+  if (isArray(value)) return value.every(isString);
+
+  // if value is an object return whether or not every value is a string
   return keys(value).every((key) => {
     return isString(key) && isString(value[key]);
   });
+
 }
 
 export function normalizeGlobals(globals: GlobalsOptions): AllowNull<Record<string, string>> {
+
+  // return null if option is nullish
   if (!globals) return null;
+
+  // return option if it's an object
   if (!isArray(globals)) return globals;
+
+  // return object from array
   return globals.reduce<Record<string, string>>((result, value) => {
     return { ...result, [value]: value };
   }, {});
+
 }
