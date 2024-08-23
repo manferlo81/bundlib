dae6398452c84ead63131d6631362ce6d061386b
Manuel Fernández <manferlo81@gmail.com>
2024-08-23 08:30:43 +0700

    chore(types): Export types by name

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,9 +4,66 @@ export { configsFromPkg } from './configs-from-pkg';
 export { config } from './functions/config';
 export { readPkg } from './package/read-pkg';
 export { pkgToConfigs } from './pkg-to-configs';
-export * from './types/bundlib-options';
-export * from './types/deprecated-exports';
-export * from './types/pkg-analyzed';
-export * from './types/pkg-json';
-export * from './types/rollup';
-export * from './types/types';
+export type {
+  BooleanOption,
+  BuildType,
+  BundlibConfig,
+  GlobalsOption,
+  OverrideKey,
+  SelectiveBoolBasedOption,
+  SelectiveEsModuleOption,
+  SelectiveInteropOption,
+  SelectiveKey,
+  SelectiveMinOption,
+  SelectiveObjectKey,
+  SelectiveSkipKey,
+  SelectiveSkipOption,
+  SelectiveSourcemapOption,
+  SelectiveSpecialKey,
+  SelectiveStringOption,
+  SelectiveValueBasedOption,
+  StringOption,
+} from './types/bundlib-options';
+export type {
+  BuildTypeForAPI,
+  GlobalsOptions,
+  SelectiveSkipBuildType,
+} from './types/deprecated-exports';
+export type {
+  BrowserBuildOptions,
+  Dependencies,
+  InstalledModules,
+  ModuleBuildOptions,
+  ModuleInstalled,
+  OptionalModules,
+  PkgAnalyzed,
+  TypesBuildOptions,
+} from './types/pkg-analyzed';
+export type {
+  BundlibPkgJson,
+  PkgJsonDependencies,
+  PkgJsonExports,
+  PkgJsonExportsObject,
+  PkgJsonExportsPath,
+  PkgJsonExportsPathsObject,
+  PkgJsonExportsValue,
+  PkgJsonModuleType,
+  PkgJsonObject,
+} from './types/pkg-json';
+export type {
+  BrowserBuildFormat,
+  BundlibBuildFormat,
+  BundlibRollupBrowseOutputOptions,
+  BundlibRollupModuleOutputOptions,
+  BundlibRollupOptions,
+  ModuleBuildFormat,
+  RollupBundlibInterop,
+  RollupEsModule,
+  RollupEsModuleString,
+  RollupInterop,
+  RollupSourcemap,
+  RollupSourcemapString,
+} from './types/rollup';
+export type {
+  BundlibAPIOptions,
+} from './types/types';
