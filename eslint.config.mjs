2055f00bbd485d2aa45b02d21118ee38c771a641
Manuel Fernández <manferlo81@gmail.com>
2024-09-05 17:50:52 +0700

    chore: Refactor eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,31 +3,37 @@ import stylistic from '@stylistic/eslint-plugin';
 import globals from 'globals';
 import { config, configs as typescriptConfigs } from 'typescript-eslint';
 
-const normalizeRuleEntry = (entry) => {
-  if (Array.isArray(entry) || ['error', 'warn', 'off'].includes(entry)) return entry;
+function normalizeRuleEntry(entry) {
+  if (Array.isArray(entry)) return entry;
+  if (['error', 'warn', 'off'].includes(entry)) return entry;
   return ['error', entry];
-};
+}
 
-const normalizePluginRuleName = (pluginName, ruleName) => {
-  if (!pluginName) return ruleName;
-  const pluginPrefix = `${pluginName}/`;
-  if (ruleName.startsWith(pluginPrefix)) return ruleName;
-  return `${pluginPrefix}${ruleName}`;
-};
+function createRuleNameNormalizer(pluginName) {
+  if (!pluginName) return (ruleName) => ruleName;
+  return (ruleName) => {
+    const pluginPrefix = `${pluginName}/`;
+    if (ruleName.startsWith(pluginPrefix)) return ruleName;
+    return `${pluginPrefix}${ruleName}`;
+  };
+}
 
-const pluginRules = (pluginName, rules) => Object.fromEntries(
-  Object.entries(rules).map(([ruleName, ruleEntry]) => {
-    return [normalizePluginRuleName(pluginName, ruleName), normalizeRuleEntry(ruleEntry)];
-  }),
-);
+function rules(pluginName, rules) {
+  const normalizeRuleName = createRuleNameNormalizer(pluginName);
+  return Object.fromEntries(
+    Object.entries(rules).map(([ruleName, ruleEntry]) => {
+      return [normalizeRuleName(ruleName), normalizeRuleEntry(ruleEntry)];
+    }),
+  );
+}
 
-const eslintRules = pluginRules(null, {
+const eslintRules = rules(null, {
   'no-useless-rename': 'error',
   'object-shorthand': 'error',
   'prefer-template': 'error',
 });
 
-const stylisticRules = pluginRules('@stylistic', {
+const stylisticRules = rules('@stylistic', {
   semi: 'always',
   indent: 2,
   quotes: 'single',
@@ -44,26 +50,29 @@ const stylisticRules = pluginRules('@stylistic', {
   'padded-blocks': 'off',
 });
 
-const typescriptRules = pluginRules('@typescript-eslint', {
+const typescriptRules = rules('@typescript-eslint', {
   'array-type': {
     default: 'array-simple',
     readonly: 'array-simple',
   },
 });
 
+const javascriptExtensions = ['js', 'cjs', 'mjs'];
+const javascriptExtString = javascriptExtensions.join(',');
+
 const typescriptFlatConfigs = config(
   ...typescriptConfigs.strictTypeChecked,
   ...typescriptConfigs.stylisticTypeChecked,
   { languageOptions: { parserOptions: { projectService: true, tsconfigRootDir: process.cwd() } } },
-  { files: ['**/*.{js,mjs,cjs}'], ...typescriptConfigs.disableTypeChecked },
+  { files: [`**/*.{${javascriptExtString}}`], ...typescriptConfigs.disableTypeChecked },
 );
 
 export default config(
   { ignores: ['bin', 'dist', 'coverage'] },
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { files: [`**/*.{${javascriptExtString},ts}`] },
   { languageOptions: { globals: { ...globals.node, ...globals.browser } } },
   js.configs.recommended,
   stylistic.configs['recommended-flat'],
   ...typescriptFlatConfigs,
-  { rules: { ...eslintRules, ...typescriptRules, ...stylisticRules } },
+  { rules: { ...eslintRules, ...stylisticRules, ...typescriptRules } },
 );
