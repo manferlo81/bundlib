5c49fe5a025b358b06959feee514bd11b45bafca
Manuel Fernández <manferlo81@gmail.com>
2024-09-10 11:59:28 +0700

    chore: Remove unused catch parameter

--- src/api/selective/resolve-or-throw.ts
+++ src/api/selective/resolve-or-throw.ts
@@ -5,8 +5,7 @@ import { invalidOptionMessage } from '../errors/error-messages';
 export function resolveOptionOrThrow<I, K extends string, V>(resolve: Resolver<K, V, I>, value: I, optionName: string): Resolved<K, V> {
   try {
     return resolve(value);
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (_) {
+  } catch {
     throw error(invalidOptionMessage(optionName));
   }
 }
