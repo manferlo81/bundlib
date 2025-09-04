75cde6247cabeeb25f0d77d4da4e07847906f88f
Manolin <manferlo81@gmail.com>
2025-09-04 21:32:27 +0700

    fix(cli): Fix bug where `build` command doesn't respect `--watch` option (#1657)
    * fix(cli): Fix bug where `build` command doesn't respect `--watch` option
    
    * chore: Cleanup

--- src/cli/command/handle-cli-args.ts
+++ src/cli/command/handle-cli-args.ts
@@ -6,10 +6,6 @@ import { booleanOptions } from './options/boolean';
 import { createFlags } from './options/flags';
 import type { ProgramOptions } from './options/option-types';
 
-const logWatchOptionsWarning = (r: string) => {
-  logWarning(`Using the ${greenBright('--watch')} option is deprecated. ${r}.`);
-};
-
 export function handleCLI(action: (options: ProgramOptions) => void | Promise<void>): void {
 
   const program = createCommand();
@@ -31,18 +27,33 @@ export function handleCLI(action: (options: ProgramOptions) => void | Promise<vo
     // get all options
     const opts = program.opts<ProgramOptions>();
 
-    // exclude `watch` option from options
-    const { watch, ...buildOptions } = opts;
-
     // warn if `watch` options is being used
-    if (watch) {
-      logWatchOptionsWarning(`Please use the ${greenBright('watch')} command to run in watch mode`);
+    if (opts.watch) {
+      logWarning(`Using the ${greenBright('--watch, -w')} option is deprecated. Please use the ${greenBright('watch')} command to run in watch mode.`);
       logInfo('');
     }
 
     // call action
-    await action(buildOptions);
+    await action(opts);
 
+  };
+
+  const watchAction = async () => {
+
+    // get all options
+    const opts = program.opts<ProgramOptions>();
+
+    // warn if `watch` options is being used
+    if (opts.watch) {
+      logWarning(`You are using the deprecated ${greenBright('--watch, -w')} option on the ${greenBright('watch')} command. We'll ignore it this time but this will fail in the future.`);
+      logInfo('');
+    }
+
+    // force `dev` and `watch` options
+    const devOptions: ProgramOptions = { dev: true, ...opts, watch: true };
+
+    // call action
+    await action(devOptions);
   };
 
   program
@@ -53,24 +64,7 @@ export function handleCLI(action: (options: ProgramOptions) => void | Promise<vo
   program
     .command('watch')
     .description('Starts Bundlib in watch mode')
-    .action(async () => {
-
-      // get all options
-      const opts = program.opts<ProgramOptions>();
-
-      // warn if `watch` options is being used
-      const { watch } = opts;
-      if (watch) {
-        logWatchOptionsWarning(`The ${greenBright('watch')} command will run in watch mode no matter what`);
-        logInfo('');
-      }
-
-      // force `dev` and `watch` options
-      const devOptions = { ...opts, dev: true, watch: true } as ProgramOptions;
-
-      // call action
-      await action(devOptions);
-    });
+    .action(watchAction);
 
   program.action(buildAction);
 
