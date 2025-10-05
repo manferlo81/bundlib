037cd2b5b85c193000c8a0f8d88810521398cab2
Manuel Fernández <manferlo81@gmail.com>
2025-10-05 16:12:19 +0700

    chore: Switched from `pretty-ms` to local implementation

--- src/cli/tools/format.ts
+++ src/cli/tools/format.ts
@@ -1,5 +1,4 @@
 import { createFormatter } from 'gen-unit';
-import prettyMs from 'pretty-ms';
 
 export const formatFileSize = createFormatter({
   unit: 'B',
@@ -14,6 +13,24 @@ export const formatFileSize = createFormatter({
   },
 });
 
-export function formatMS(duration: number) {
-  return prettyMs(duration, { secondsDecimalDigits: 2 });
+export const formatSeconds = createFormatter({
+  unit: 's',
+  round: 2,
+  find: [
+    { exp: -1, pre: 'm' },
+    { exp: 0, pre: '' },
+  ],
+  output: { space: 0 },
+});
+
+function formatMinutes(minutes: number) {
+  const min = Math.floor(minutes);
+  const sec = Math.round((minutes - min) * 60);
+  if (!sec) return `${min}m`;
+  return `${min}m ${sec}s`;
+}
+
+export function formatMS(milliseconds: number): string {
+  if (milliseconds < 60000) return formatSeconds(milliseconds / 1000);
+  return formatMinutes(milliseconds / 60000);
 }
