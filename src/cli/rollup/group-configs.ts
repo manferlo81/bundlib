3b57f640e4526ec208215b9523ad0298c2f2799c
Manolin <manferlo81@gmail.com>
2025-10-08 17:10:00 +0700

    fix: Group configs by module types as well as input file (#1688)
    * fix: Group configs by module types as well as input file
    
    Fixes #1687
    
    * chore: Review README

--- src/cli/rollup/group-configs.ts
+++ src/cli/rollup/group-configs.ts
@@ -5,9 +5,10 @@ export function groupConfigs(configs: BundlibRollupConfig[]) {
   // Group configs to process some of them concurrently
   const configGroupsMap = configs.reduce<Record<string, BundlibRollupConfig[]>>((configsMap, config) => {
 
-    // Use input file as group key
-    const { input } = config
-    const configSetKey = input
+    // Use input file and format as group key
+    const { input, output: { format } } = config
+    const formatKey = format === 'es' || format === 'cjs' ? 'module' : 'browser'
+    const configSetKey = `${formatKey}:${input}`
 
     const currentConfigList = configsMap[configSetKey] as BundlibRollupConfig[] | undefined
     const configList = currentConfigList ? [...currentConfigList, config] : [config]
